678c90c2cc67192a7cbd292be29aa97b2db798c9
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-01-10 06:38:34 +0100

    changed background file select to maxdepth 1 to prevent .git being considered

--- x-desktop-scripts/.local/bin/set_random_wallpaper.sh
+++ x-desktop-scripts/.local/bin/set_random_wallpaper.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Define the directory containing the images
 WALLPAPER_DIR="$HOME/Bilder/backgrounds/"
 
-# Select a random image from the directory
-SELECTED_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+# Select a random image from the directory (only top level, only image files)
+SELECTED_WALLPAPER=$(find "$WALLPAPER_DIR" -maxdepth 1 -type f -regex '.*\.\(jpg\|jpeg\|png\|bmp\|tiff\|svg\)$' | shuf -n 1)
 
 # Set the selected image as the background using feh
-feh --bg-scale "$SELECTED_WALLPAPER"
+feh --bg-fill "$SELECTED_WALLPAPER"
 
