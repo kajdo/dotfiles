68fed7b856827f8edfe8e6efc18a7ab431103c95
Juergen Kajdocsy <juergen.kajdocsy@gmail.com>
2025-01-19 18:35:19 +0100

    added pyradio

--- x-desktop-scripts/.local/bin/dwm_autostart_cmd.sh
+++ x-desktop-scripts/.local/bin/dwm_autostart_cmd.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 # Starts when DWM is initialy loaded
-set_random_wallpaper.sh
+set_random_wallpaper.sh &
+
+# start dwmblocks
+dwmblocks &
+
+# start compositor
+picom --config ~/.config/picom/picom.conf -b
+
+# start dunst
+dunst &
